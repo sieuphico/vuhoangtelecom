671e19a847f20583d0cb450c6dd793db732c0190
sonnt3 <truongsontn94@gmail.com>
2021-11-02 22:06:13 +0700

    add reactjs

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <div></div>
+      <img src={logo} alt="hello" width="200" height="200"></img>
+    </div>
+  );
+}
+
+export default App;
